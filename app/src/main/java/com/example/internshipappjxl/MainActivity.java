6da23bcdf9695bf10c9742411a2d8354c50b8737
fork52 <mrunankmistry52@gmail.com>
2019-06-02 19:19:10 +0530

    Proper Scrolling of Text

--- app/src/main/java/com/example/internshipappjxl/MainActivity.java
+++ app/src/main/java/com/example/internshipappjxl/MainActivity.java
@@ -19,36 +19,105 @@ import jxl.Workbook;
 
 public class MainActivity extends AppCompatActivity {
     TextView tv;
+    int CurrentRowNo; //To keep a track of the current being accessed
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         tv = (TextView)findViewById(R.id.TEXTDISP);
-    }
+        Toast.makeText(this,"OnCreate",Toast.LENGTH_SHORT).show();
 
-    public void btnClick(android.view.View view){
-        try {
+
+        try{
+
+            //Displaying the contents of the first entry
             AssetManager am = getAssets();
             InputStream is  = am.open("Sample.xls");
             Workbook wb = Workbook.getWorkbook(is);
             Sheet s = wb.getSheet(0);
 
-            int row = s.getRows();
-            int col = s.getColumns();
 
-            String data ="";
+            Toast.makeText(this,"Making access",Toast.LENGTH_SHORT).show();
+            CurrentRowNo = 0;
+            Cell nameCell = s.getCell(0,0);
+            Cell phonenoCell =s.getCell(1,0);
 
-            for(int i=0;i<row;i++){
-                for(int j=0;j<col;j++){
-                   Cell c = s.getCell(j,i);
-                   data = data + " " + c.getContents().toString();
-                }
-                data = data + "\n";
-            }//end of outer for
-            tv.setText(data);
+            String tobedisplayed = "Name:"+nameCell.getContents() +"\nPhoneno:"+phonenoCell.getContents();
 
-            Toast.makeText(this,"Access to Excel file Successful",Toast.LENGTH_LONG).show();
+            tv.setText(tobedisplayed);
+
+
+            wb.close();
+            is.close();
+        }
+        catch (Exception e){
+
+        }
+
+
+    }//end of Oncreate
+
+    public void MakeCall(android.view.View view){
+        String callString ="tel:";
+
+        try{
+
+            //Displaying the contents of the first entry
+            AssetManager am = getAssets();
+            InputStream is  = am.open("Sample.xls");
+            Workbook wb = Workbook.getWorkbook(is);
+            Sheet s = wb.getSheet(0);
+
+
+            Cell nameCell = s.getCell(0,CurrentRowNo);
+            Cell phonenoCell =s.getCell(1,CurrentRowNo);
+
+            callString = callString + phonenoCell.getContents();
+
+            String tobedisplayed = "Name:"+nameCell.getContents() +"\nPhoneno:"+phonenoCell.getContents();
+
+            tv.setText(tobedisplayed);
+
+
+            wb.close();
+            is.close();
+        }
+        catch (Exception e){
+
+        }
+
+
+
+
+//        Intent callIntent = new Intent(Intent.ACTION_CALL);
+//        callIntent.setData(Uri.parse(callString));
+//        startActivity(callIntent);
+
+    }
+
+    public void btnClickLeft(android.view.View view){
+        try {
+            Toast.makeText(this,"Inside btnClik",Toast.LENGTH_SHORT).show();
+            AssetManager am = getAssets();
+            InputStream is  = am.open("Sample.xls");
+            Workbook wb = Workbook.getWorkbook(is);
+            Sheet s = wb.getSheet(0);
+
+            int MaxEntries = s.getRows();
+
+            CurrentRowNo = (CurrentRowNo-1+MaxEntries)%MaxEntries; //To be Modified
+
+            Cell nameCell = s.getCell(0,CurrentRowNo);
+            Cell phonenoCell =s.getCell(1,CurrentRowNo);
+
+            String tobedisplayed = "Name:"+nameCell.getContents() +"\nPhoneno:"+phonenoCell.getContents();
+
+            tv.setText(tobedisplayed);
+
+            wb.close();
+            is.close();
         }
         catch (Exception e){
 
@@ -68,5 +137,38 @@ public class MainActivity extends AppCompatActivity {
 //        }
 
 
-    }//method
+    }//method btnClick
+
+
+
+
+
+    public void btnClickRight(android.view.View view) {
+        try {
+            AssetManager am = getAssets();
+            InputStream is = am.open("Sample.xls");
+            Workbook wb = Workbook.getWorkbook(is);
+            Sheet s = wb.getSheet(0);
+
+            int MaxEntries = s.getRows();
+
+            CurrentRowNo = (CurrentRowNo + 1 ) % MaxEntries; //To be Modified
+
+            Cell nameCell = s.getCell(0, CurrentRowNo);
+            Cell phonenoCell = s.getCell(1, CurrentRowNo);
+
+            String tobedisplayed = "Name:" + nameCell.getContents() + "\nPhoneno:" + phonenoCell.getContents();
+
+            tv.setText(tobedisplayed);
+
+            wb.close();
+            is.close();
+
+        } catch (Exception e) {
+
+        }//catch
+
+    }//btnClickRight
+
+
 }
